c03f756348fab46bd6db5dd595caa952c843451e
asadabbasse2006 <asadse2006@gmail.com>
2026-03-05 23:40:09 +0500

    Updated

--- Regular Expressions/Matching.py
+++ Regular Expressions/Matching.py
@@ -1,11 +1,30 @@
 import re
 
 text = 'I am Asad Abbas'
+
+# It will print the list type with am exist in it. If am not exists, it will print the None.
+
 matching = re.findall('am', text)
 print(matching)
 
-test = re.search('Asad', text)
-print(test)
+# It will print the true if the text exists else false
 
 test = re.search('Asad', text)
 print(test)
+
+# It will print the None because it is not starting with am
+
+test = re.search('^am',text)
+print(test)
+
+# It will print the true because it is starting with I
+
+test = re.search('^I',text)
+print(test)
+
+pattern = r'[aeiouAEIOU]'
+test = re.findall(pattern, 'education')
+print(test)
+
+text = 'Hi, I am Asad Abbas'
+print(re.findall(pattern, text))
